de5ddeb511b4f482d9598d90bd39098bc2bdc7cb
Viktoria <vikakib2005@gmail.com>
2021-08-19 13:23:33 +0300

    add fun generateLife and printBackground

--- MyGame/MyGame.cpp
+++ MyGame/MyGame.cpp
@@ -1,9 +1,11 @@
 ﻿#include <iostream>
+#include <cstdlib>
+#include <ctime>
 
 using namespace std;
 
 #define spaceX 20
-#define spaceY 20
+#define spaceY 40
 
 const int X = spaceX + 2;
 const int Y = spaceY + 2;
@@ -23,13 +25,40 @@ public:
             }
         }
     }
-    ~Life();
+    
+
+    void generateLife()
+    {
+        for (int i = 1; i < X-1; i++)
+        {
+            for (int j = 1; j < Y-1; j++)
+            {
+                background[i][j] = rand() % 2;
+            }
+        }
+    }
+    void printBackground()
+    {
+        for (int i = 1; i < X - 1; i++)
+        {
+            for (int j = 1; j < Y - 1; j++)
+            {
+                if (background[i][j] == 1)
+                    cout << "*";
+                else
+                    cout << " ";
+            }
+            cout << endl;
+        }
+    }
 
 };
 
 int main()
 {
-    
+    Life life;
+    life.generateLife();
+    life.printBackground();
 
     return 0;
 }
