a956777271d6d3a8a3387248bf292844af9ee27f
Viktoria <vikakib2005@gmail.com>
2021-08-19 14:43:06 +0300

    chang rules and add LifeGo

--- MyGame/MyGame.cpp
+++ MyGame/MyGame.cpp
@@ -15,24 +15,32 @@ class Life
 {
     int background[X][Y];
 
-public:
-    Life()
+    int checkRules(int x, int y)
     {
-        for (int i = 0; i < X; i++)
-        {
-            for (int j = 0; j < Y; j++)
-            {
-                background[i][j] = 0;
-            }
-        }
-    }
-    
+        int Count = 0;
+        for (int i = x - 1; i < x + 2; i++)
+            for (int j = y - 1; j < y + 2; j++)
+                Count += background[i][j];
 
-    void generateLife()
-    {
-        for (int i = 1; i < X-1; i++)
-            for (int j = 1; j < Y-1; j++)
-                background[i][j] = rand() % 2;
+        Count -= background[x][y];
+
+
+        //якщо в мертвої клітини рівно три живих сусіди то вона оживає
+        if (Count == 3 && background[x][y] == 0)
+            background[x][y] = 1;
+
+        //якщо в живої клітини два чи три живих сусіди то вона лишається жити
+        //якщо в живої клітини один чи немає живих сусідів то вона помирає від самотності
+        //якщо в живої клітини чотири та більше живих сусідів вона помирає відперенаселення
+        if ((Count > 3 && background[x][y] == 1) || (Count < 2 && background[x][y] == 1))
+            background[x][y] = 0;
+
+
+
+
+
+
+        return background[x][y];
     }
 
     void printBackground()
@@ -49,55 +57,53 @@ public:
             cout << endl;
         }
     }
-    int checkRules(int x, int y)
+
+    void generateLife()
     {
-        int Count = 0;
-        for (int i = x - 1; i < x + 2; i++)
-            for (int j = y - 1; j < y + 2; j++)
-                Count += background[i][j];
-
-        Count -= background[x][y];
-
-        //якщо в живої клітини два чи три живих сусіди то вона лишається жити
-        if ((Count == 2 || Count == 3) && background[x][y] == 1)
-            background[x][y] == 1;
-
-        //якщо в живої клітини один чи немає живих сусідів то вона помирає від самотності
-        if ((Count == 1  || Count == 0) && background[x][y] == 1)
-            background[x][y] == 0;
-
-        //якщо в живої клітини чотири та більше живих сусідів вона помирає відперенаселення
-        if (Count <= 4 && background[x][y] == 1)
-            background[x][y] == 0;
-
-        //якщо в мертвої клітини рівно три живих сусіди то вона оживає
-        if (Count == 3 && background[x][y] == 0)
-            background[x][y] == 1;
-
-
-
-
-        return background[x][y];
+        for (int i = 1; i < X - 1; i++)
+            for (int j = 1; j < Y - 1; j++)
+                background[i][j] = rand() % 2;
     }
+
+public:
+    Life()
+    {
+        for (int i = 0; i < X; i++)
+        {
+            for (int j = 0; j < Y; j++)
+            {
+                background[i][j] = 0;
+            }
+        }
+    }
+    
+    void LifeGo()
+    {
+        generateLife();
+        printBackground();
+        Sleep(1000);
+        system("cls");
+        while (1)
+        {
+            for (int i = 1; i < X - 1; i++)
+                for (int j = 1; j < Y - 1; j++)
+                    checkRules(i, j);
+            printBackground();
+            Sleep(5);
+            system("cls");
+        }
+    }
+    
+
+   
+    
 
 };
 
 int main()
 {
     Life life;
-    life.generateLife();
-    life.printBackground();
-    Sleep(1000);
-    system("cls");
-    while (1)
-    {
-        for (int i = 1; i < X - 1; i++)
-            for (int j = 1; j < Y - 1; j++)
-                life.checkRules(i, j);
-        life.printBackground();
-        Sleep(5);
-        system("cls");
-    }
+    life.LifeGo();
 
 
     return 0;
